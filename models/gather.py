e0e227a2aeb4d5a8d918229df80d38881180f65e
cloveses <cloveses@163.com>
2018-05-28 17:51:18 +0800

    m

--- models/gather.py
+++ models/gather.py
@@ -6,19 +6,23 @@ def clear_data():
     for o in (PlayGround,Games,Player,Team,PlayDate,Face,Group)[::-1]:
         select(r for r in o).delete(bulk=True)
 
+# def exvl2int(data):
+    
+
 @db_session
 def load_data(fname):
     print(fname)
     names_objs = (('playground',PlayGround,('name','memo')),
-        ('games',Games,('team_num','team_num','memo')),
+        ('games',Games,('name','team_num','memo')),
         ('players',Player,('name','idcode','sex','age','work_place','tel')))
-    if fname.startswith('.xls') or fname.startswith('.xlsx'):
+    if fname.endswith('.xls') or fname.endswith('.xlsx'):
         wb = xlrd.open_workbook(fname)
         for sheetname,obj,keys in names_objs:
             datas = []
             ws = wb.sheet_by_name(sheetname)
-            for i in range(1,ws.ws.nrows):
+            for i in range(1,ws.nrows):
                 datas.append(ws.row_values(i))
+            print(datas)
             for data in datas:
                 param = dict()
                 for d,k in zip(data,keys):

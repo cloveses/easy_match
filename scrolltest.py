6da8da4d8d4b819c7048d1af867e43984123a0a6
djx <1181564186@qq.com>
2018-06-05 21:41:34 +0800

    e

--- scrolltest.py
+++ scrolltest.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QMainWindow, QScrollArea, QLabel,QWidget, QApplication
+from PyQt5.QtWidgets import QTableWidgetItem,QTableWidget,QMainWindow, QScrollArea, QLabel,QWidget, QApplication
 from PyQt5.QtGui import QColor,QPalette
 import sys
 
@@ -10,13 +10,24 @@ class MyUi(QMainWindow):
         self.show()
 
     def setupUi(self):
-        wdgt = QWidget()
-        wdgt.setStyleSheet('QWidget{background-color:rgb(255,255,255)}')
-        wdgt.setMinimumSize(1500,800)
+        self.wdgt = QTableWidget(15,10)
+
+        self.wdgt.setItem(2,3,QTableWidgetItem('abc'))
+        self.wdgt.setItem(2,4,QTableWidgetItem('edes'))
+        self.wdgt.setItem(2,5,QTableWidgetItem('eee'))
+        self.wdgt.setStyleSheet('QWidget{background-color:rgb(255,255,255)}')
+        self.wdgt.setMinimumSize(1500,800)
+
+        self.wdgt.cellChanged.connect(self.edit_cell)
 
         scroll = QScrollArea(self)
-        scroll.setWidget(wdgt)
+        scroll.setWidget(self.wdgt)
         self.setCentralWidget(scroll)
+
+    def edit_cell(self,row,col):
+        item = self.wdgt.item(row,col)
+        txt = item.text()
+        print(txt)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
@@ -24,4 +35,9 @@ if __name__ == '__main__':
     ex.show()
     sys.exit(app.exec_())
 
-        
+# self.tableWidget.item(rowindex, colindex).text()   
+# row_count = self.table.rowCount()
+# self.table.insertRow(row_count)
+
+# row_count = self.table.rowCount()
+# self.table.removeRow(row_count-1)

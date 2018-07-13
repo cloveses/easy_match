6b581ca36522bb1383e476b0eff03933a673d967
cloveses <cloveses@163.com>
2018-07-13 18:35:31 +0800

    m

--- models/mydb.py
+++ models/mydb.py
@@ -29,21 +29,21 @@ class Player(db.Entity):
 class Team(db.Entity):
     name = Required(str)
     players = Set('Player')
-    facea = Set('Face', reverse="teama")
-    faceb = Set('Face', reverse="teamb")
+    facea = Optional('Face', reverse="teama")
+    faceb = Optional('Face', reverse="teamb")
     game = Required('Games')
     group = Optional('Group')
 
 class PlayDate(db.Entity):
     flag = Required(str)
 
-# class Face(db.Entity):
-#     teama = Required(Team)
-#     teamb = Required(Team)
-#     times = Optional(int,default=0)
-#     scorea = Optional(int,nullable=True)
-#     scoreb = Optional(int,nullable=True)
-#     playground = Optional(PlayGround)
+class Face(db.Entity):
+    teama = Required(Team, reverse="facea")
+    teamb = Required(Team, reverse="faceb")
+    times = Optional(int,default=0)
+    scorea = Optional(int,nullable=True)
+    scoreb = Optional(int,nullable=True)
+    playground = Optional(PlayGround)
 
 class Group(db.Entity):
     name = Required(str)
